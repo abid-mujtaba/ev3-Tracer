3319070e05027fbd62d0a640228c36a07c24f140
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2014-01-19 16:03:14 -0600

    Initial skeleton of project, in Hello World phase.

---
+++ src/Tracer.java
@@ -0,0 +1,17 @@
+/**
+ * Main class for a LeJOS EV3 robot with a pencil/pen attached behind it tracing its path.
+ */
+
+public class Tracer
+{
+    public static void main(String[] args)
+    {
+       log("Hello, World");
+    }
+
+
+    private static void log(String message)
+    {
+        System.out.println("log>\t" + message);
+    }
+}
